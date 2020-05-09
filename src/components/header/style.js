04ae98422b996a9801b602a783db2efe2672e411
safaa <khadim.safaa@gmail.com>
2020-05-09 00:13:56 +0000

    add the header

---
+++ src/components/header/style.js
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Image from '../../images/gametheory.png';
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    toolbar: {
+        minHeight: 528,
+        alignItems: 'flex-start',
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        alignSelf: 'flex-end',
+    },
+    paperContainer: {
+        backgroundImage: `url(${Image})`
+    },
+}));
