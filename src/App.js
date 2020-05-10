a2d65658eb61e8f09b7b18fcb201dae4dcb882c8
safaa <khadim.safaa@gmail.com>
2020-05-10 19:04:56 +0000

    nested grid

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import HideAppBar from "./components/header/HideAppBar/HideAppBar";
 import ButtonAppBar from "./components/header/ButtonAppBar/ButtonAppBar";
+import NestedGrid from "./components/post/Grid/NestedGrid";
+
+
 
 function App() {
+
   return (
     <div className="App">
       <HideAppBar></HideAppBar>
       <ButtonAppBar></ButtonAppBar>
+        <br /> <br />
+      <NestedGrid></NestedGrid>
     </div>
   );
 }
